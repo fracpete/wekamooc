814b7dbf56f6f448f0ea2b55f8971460767cfac0
Peter Reutemann <fracpete@gmail.com>
2014-05-03 14:38:55 +1200

    added groovy script and additional dataset

---
+++ dataminingwithweka/groovy/README.md
@@ -0,0 +1,18 @@
+Data Mining with Weka - Groovy
+==============================
+
+In order to run these scripts, you must download Groovy to start with:
+
+http://docs.codehaus.org/display/GROOVY/Download
+
+And then place the Weka 3.6.10 jar in the classpath when starting
+up Groovy. The following Groovy article describes how to configure
+Groovy and where to place the jars:
+
+http://groovy.codehaus.org/Tutorial+1+-+Getting+started
+
+You can start up the Groovy Console from the `bin` directory of
+your Groovy installation:
+
+`groovyConsole`
+
---
+++ dataminingwithweka/groovy/class-2.4.groovy
@@ -0,0 +1,83 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Data Mining with Weka - Class 2.4
+ * Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+ */
+
+import java.io.File
+import java.util.Random
+import weka.classifiers.Classifier
+import weka.classifiers.Evaluation
+import weka.core.converters.ConverterUtils.DataSource
+import weka.core.Instances
+
+// TODO
+// wherever your datasets are located
+data_dir = "/some/where/data"
+
+classifiers = [
+    new weka.classifiers.rules.ZeroR(),
+    new weka.classifiers.trees.J48(),
+    new weka.classifiers.bayes.NaiveBayes(),
+    new weka.classifiers.lazy.IBk(),
+    new weka.classifiers.rules.PART()
+]
+percent = 66.0
+
+// load diabetes
+data = data_dir + File.separator + "diabetes.arff"
+printf("\nLoading dataset: " + data + "\n")
+data = DataSource.read(data)
+data.setClassIndex(data.numAttributes() - 1)
+
+// random split on data
+rnd = new Random(1)
+data.randomize(rnd)
+train_size = (int) (data.numInstances() * percent / 100)
+test_size = data.numInstances() - train_size
+train = new Instances(data, 0, train_size)
+test = new Instances(data, train_size, test_size)
+
+// train + evaluate
+for (classifier in classifiers) {
+  evl = new Evaluation(data)
+  classifier.buildClassifier(train)
+  evl.evaluateModel(classifier, test)
+  printf(classifier.getClass().getName() + ": %02.0f\n", evl.pctCorrect())
+}
+
+// load supermarket
+data = data_dir + File.separator + "supermarket.arff"
+printf("\nLoading dataset: " + data + "\n")
+data = DataSource.read(data)
+data.setClassIndex(data.numAttributes() - 1)
+
+// random split on data
+rnd = new Random(1)
+data.randomize(rnd)
+train_size = (int) (data.numInstances() * percent / 100)
+test_size = data.numInstances() - train_size
+train = new Instances(data, 0, train_size)
+test = new Instances(data, train_size, test_size)
+
+// train + evaluate
+for (classifier in classifiers) {
+  evl = new Evaluation(data)
+  classifier.buildClassifier(train)
+  evl.evaluateModel(classifier, test)
+  printf(classifier.getClass().getName() + ": %02.0f\n", evl.pctCorrect())
+}
