6cfcf9664a16a06a27cbc12a35c4168fbc7e5207
Peter Reutemann <fracpete@gmail.com>
2014-05-05 21:49:59 +1200

    lesson 2.3

---
+++ dataminingwithweka/class-2.3.py
@@ -0,0 +1,50 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Data Mining with Weka - Class 2.3
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+import os
+import numpy
+import weka.core.jvm as jvm
+from weka.core.converters import Loader
+from weka.core.classes import Random
+from weka.classifiers import Classifier, Evaluation
+
+jvm.start()
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+# load segment-challenge
+loader = Loader(classname="weka.core.converters.ArffLoader")
+
+fname = data_dir + os.sep + "segment-challenge.arff"
+print("\nLoading dataset: " + fname + "\n")
+train = loader.load_file(fname)
+train.set_class_index(train.num_attributes() - 1)
+
+# use seed 1-10 and perform random split with 90%
+perc = []
+for i in xrange(1, 11):
+    evl = Evaluation(train)
+    evl.evaluate_train_test_split(Classifier("weka.classifiers.trees.J48"), train, 90.0, Random(i))
+    perc.append(round(evl.percent_correct(), 1))
+    print("Accuracy with seed %i: %0.1f%%" % (i, evl.percent_correct()))
+
+# calculate mean and standard deviation
+nperc = numpy.array(perc)
+print("mean=%0.2f stdev=%0.2f" % (numpy.mean(nperc), numpy.std(nperc)))
+
+jvm.stop()
