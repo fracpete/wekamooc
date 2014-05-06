e1aae94c670d7ab7685d9228c915e0c57bdb2993
Peter Reutemann <fracpete@gmail.com>
2014-05-06 12:10:48 +1200

    lesson 3.4

---
+++ dataminingwithweka/class-3.4.py
@@ -0,0 +1,42 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Data Mining with Weka - Class 3.4
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+import os
+import weka.core.jvm as jvm
+from weka.core.converters import Loader
+from weka.classifiers import Classifier
+import weka.plot.graph as plg
+
+jvm.start()
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+# load weather.nominal
+loader = Loader(classname="weka.core.converters.ArffLoader")
+fname = data_dir + os.sep + "weather.nominal.arff"
+print("\nLoading dataset: " + fname + "\n")
+data = loader.load_file(fname)
+data.set_class_index(data.num_attributes() - 1)
+
+# build J48 and display tree
+cls = Classifier(classname="weka.classifiers.trees.J48")
+cls.build_classifier(data)
+print("Model:\n\n" + str(cls))
+plg.plot_dot_graph(cls.graph())
+
+jvm.stop()
