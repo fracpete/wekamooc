6dc37c4ae7d91dec5b90842ff96fbfeed4b3f1df
Peter Reutemann <fracpete@gmail.com>
2014-05-20 15:57:51 +1200

    lesson 1.3

---
+++ moredataminingwithweka/class-1.3.py
@@ -0,0 +1,112 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# More Data Mining with Weka - Class 1.3
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+import os
+import tempfile
+import weka.core.jvm as jvm
+import weka.core.converters as converters
+from weka.experiments import SimpleCrossValidationExperiment, SimpleRandomSplitExperiment, ResultMatrix, Tester
+from weka.classifiers import Classifier
+
+jvm.start()
+
+# setup cross-validation experiment and run it
+datasets = [
+    data_dir + os.sep + "iris.arff"
+]
+classifiers = [
+    Classifier(classname="weka.classifiers.trees.J48"),
+    Classifier(classname="weka.classifiers.rules.OneR"),
+    Classifier(classname="weka.classifiers.rules.ZeroR")
+]
+outfile = tempfile.gettempdir() + os.sep + "results-cv1.arff"
+exp = SimpleCrossValidationExperiment(
+    datasets=datasets,
+    classifiers=classifiers,
+    folds=10,
+    runs=10,
+    result=outfile
+)
+exp.setup()
+exp.run()
+
+# evaluate experiment
+loader = converters.loader_for_file(outfile)
+data = loader.load_file(outfile)
+matrix = ResultMatrix(classname="weka.experiment.ResultMatrixPlainText")
+tester = Tester(classname="weka.experiment.PairedCorrectedTTester")
+comparison_col = data.get_attribute_by_name("Percent_correct").get_index()
+tester.set_instances(data)
+tester.set_resultmatrix(matrix)
+print(tester.header(comparison_col))
+print(tester.multi_resultset_full(0, comparison_col))
+
+# setup cross-validation experiment and run it
+datasets = [
+    data_dir + os.sep + "iris.arff",
+    data_dir + os.sep + "breast-cancer.arff",
+    data_dir + os.sep + "credit-g.arff",
+    data_dir + os.sep + "diabetes.arff",
+    data_dir + os.sep + "glass.arff",
+    data_dir + os.sep + "ionosphere.arff",
+    data_dir + os.sep + "segment-challenge.arff",
+]
+classifiers = [
+    Classifier(classname="weka.classifiers.trees.J48"),
+    Classifier(classname="weka.classifiers.rules.OneR"),
+    Classifier(classname="weka.classifiers.rules.ZeroR")
+]
+outfile = tempfile.gettempdir() + os.sep + "results-cv2.arff"
+exp = SimpleCrossValidationExperiment(
+    datasets=datasets,
+    classifiers=classifiers,
+    folds=10,
+    runs=10,
+    result=outfile
+)
+exp.setup()
+exp.run()
+
+# evaluate experiment (1)
+loader = converters.loader_for_file(outfile)
+data = loader.load_file(outfile)
+matrix = ResultMatrix(classname="weka.experiment.ResultMatrixPlainText")
+tester = Tester(classname="weka.experiment.PairedCorrectedTTester")
+comparison_col = data.get_attribute_by_name("Percent_correct").get_index()
+tester.set_instances(data)
+tester.set_resultmatrix(matrix)
+print(tester.header(comparison_col))
+print(tester.multi_resultset_full(0, comparison_col))
+
+# evaluate experiment (2) with datasets/classifiers swapped
+loader = converters.loader_for_file(outfile)
+data = loader.load_file(outfile)
+matrix = ResultMatrix(classname="weka.experiment.ResultMatrixPlainText")
+tester = Tester(classname="weka.experiment.PairedCorrectedTTester")
+comparison_col = data.get_attribute_by_name("Percent_correct").get_index()
+tester.set_instances(data)
+tester.set_resultmatrix(matrix)
+tester.set_dataset_columns(["Key_Scheme", "Key_Scheme_options", "Key_Scheme_version_ID"])
+tester.set_result_columns(["Key_Dataset"])
+print(tester.header(comparison_col))
+print(tester.multi_resultset_full(0, comparison_col))
+
+jvm.stop()
+
