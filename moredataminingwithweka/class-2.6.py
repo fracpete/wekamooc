ff0bca90af7ec0820796e314084d7189cfeb0a6d
Peter Reutemann <fracpete@gmail.com>
2014-05-21 11:26:30 +1200

    lesson 2.6

---
+++ moredataminingwithweka/class-2.6.py
@@ -0,0 +1,67 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# More Data Mining with Weka - Class 2.6
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+import os
+import weka.core.jvm as jvm
+from weka.core.converters import Loader
+from weka.classifiers import FilteredClassifier, Classifier, Evaluation
+from weka.filters import Filter
+import weka.plot.classifiers as plc
+
+jvm.start()
+
+# load ReutersGrain-train
+fname = data_dir + os.sep + "ReutersGrain-train.arff"
+print("\nLoading dataset: " + fname + "\n")
+loader = Loader(classname="weka.core.converters.ArffLoader")
+data = loader.load_file(fname)
+data.set_class_index(data.num_attributes() - 1)
+
+# load ReutersGrain-train
+fname = data_dir + os.sep + "ReutersGrain-test.arff"
+print("\nLoading dataset: " + fname + "\n")
+loader = Loader(classname="weka.core.converters.ArffLoader")
+test = loader.load_file(fname)
+test.set_class_index(test.num_attributes() - 1)
+
+setups = (
+    ("weka.classifiers.trees.J48", []),
+    ("weka.classifiers.bayes.NaiveBayes", []),
+    ("weka.classifiers.bayes.NaiveBayesMultinomial", []),
+    ("weka.classifiers.bayes.NaiveBayesMultinomial", ["-C"]),
+    ("weka.classifiers.bayes.NaiveBayesMultinomial", ["-C", "-L", "-S"])
+)
+
+# cross-validate classifiers
+for setup in setups:
+    classifier, opt = setup
+    print("\n--> %s (filter options: %s)\n" % (classifier, " ".join(opt)))
+    cls = FilteredClassifier()
+    cls.set_classifier(Classifier(classname=classifier))
+    cls.set_filter(Filter(classname="weka.filters.unsupervised.attribute.StringToWordVector", options=opt))
+    cls.build_classifier(data)
+    evl = Evaluation(test)
+    evl.test_model(cls, test)
+    print("Accuracy: %0.0f%%" % evl.percent_correct())
+    tcdata = plc.generate_thresholdcurve_data(evl, 0)
+    print("AUC: %0.3f" % plc.get_auc(tcdata))
+    print(evl.to_matrix("Matrix:"))
+
+jvm.stop()
