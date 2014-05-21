d46aae4564c234874299c98787578be20bfad8d0
Peter Reutemann <fracpete@gmail.com>
2014-05-21 15:10:54 +1200

    lesson 3.3

---
+++ moredataminingwithweka/class-3.3.py
@@ -0,0 +1,40 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# More Data Mining with Weka - Class 3.3
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+import os
+import weka.core.jvm as jvm
+from weka.core.converters import Loader
+from weka.associations import Associator
+
+jvm.start()
+
+# load weather.nominal
+fname = data_dir + os.sep + "weather.nominal.arff"
+print("\nLoading dataset: " + fname + "\n")
+loader = Loader(classname="weka.core.converters.ArffLoader")
+data = loader.load_file(fname)
+data.set_class_index(data.num_attributes() - 1)
+
+# build Apriori
+assoc = Associator(classname="weka.associations.Apriori")
+assoc.build_associations(data)
+print(assoc)
+
+jvm.stop()
