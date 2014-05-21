7d55de288012b7f48fadf2bbe83079ebdc4fb64b
Peter Reutemann <fracpete@gmail.com>
2014-05-21 14:46:13 +1200

    lesson 3.1

---
+++ moredataminingwithweka/class-3.1.py
@@ -0,0 +1,60 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# More Data Mining with Weka - Class 2.6
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+# TODO
+# wherever your datasets are located
+data_dir = "/some/where/data"
+
+import os
+import weka.core.jvm as jvm
+import weka.core.packages as packages
+from weka.core.converters import Loader
+from weka.core.classes import Random
+from weka.classifiers import Classifier, Evaluation
+
+jvm.start(packages=True)
+
+pkg = "simpleEducationalLearningSchemes"
+if not packages.is_installed(pkg):
+    if not packages.install_package(pkg):
+        raise Exception("Failed to install package: " + pkg)
+    else:
+        print("Package '" + pkg + "', please restart!")
+        exit()
+
+# load weather.nominal
+fname = data_dir + os.sep + "weather.nominal.arff"
+print("\nLoading dataset: " + fname + "\n")
+loader = Loader(classname="weka.core.converters.ArffLoader")
+data = loader.load_file(fname)
+data.set_class_index(data.num_attributes() - 1)
+
+# cross-validate classifiers
+classifiers = [
+    "weka.classifiers.trees.J48",
+    "weka.classifiers.rules.Prism"
+]
+for classifier in classifiers:
+    print("\n--> %s\n" % classifier)
+    cls = Classifier(classname=classifier)
+    cls.build_classifier(data)
+    evl = Evaluation(data)
+    evl.crossvalidate_model(cls, data, 10, Random(1))
+    cls.build_classifier(data)
+    print(cls)
+    print(evl.to_summary())
+
+jvm.stop()
